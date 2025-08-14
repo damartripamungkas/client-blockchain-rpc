6637f9b901b838a23159596c06f48ea479f2aef9
damartripamungkas <damartripamungkas@gmail.com>
2025-08-14 18:33:38 +0700

    test: update test

---
+++ test/test.ts
@@ -0,0 +1,38 @@
+import { Provider } from "../dist/src/"
+const provider = new Provider(`wss://ethereum-rpc.publicnode.com`)
+
+const run = async () => {
+  console.log(`============= test .send()`)
+  const blockNumber = await provider.send({ method: `eth_blockNumber`, params: [], formatReturn: BigInt })
+  console.log(`============= test .send():`, blockNumber)
+
+  console.log(`============= test .sendBatch()`)
+  const req = await provider.sendBatch([
+    {
+      method: `eth_blockNumber`,
+      params: [],
+      formatReturn: BigInt
+    },
+    {
+      method: `eth_chainId`,
+      params: [],
+      formatReturn: parseInt
+    }
+  ])
+  console.log(`============= test .sendBatch():`, req)
+
+  let id = null as any
+  console.log(`============= test .subscribe()`)
+  provider.subscribe({ method: `eth_subscribe`, params: [`newHeads`] }, true, (data, subsId) => {
+    if (!id) {
+      id = subsId
+      console.log(`============= test .subscribe():`, subsId)
+      return
+    }
+
+    console.log(`============= test .subscribe():`, data)
+    process.exit()
+  })
+}
+
+run()
