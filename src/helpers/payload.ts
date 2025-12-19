be35298a5bf63d62c974c77c64ce0e3dd36957b9
damartripamungkas <damartripamungkas@gmail.com>
2025-12-19 16:54:59 +0700

    feat: add function reformat for change format return from payload builder

--- src/helpers/payload.ts
+++ src/helpers/payload.ts
@@ -2,4 +2,11 @@ export class PayloadHelper {
   public static build<T = any>(method: string, params: any[] = [], format?: (raw: any) => T) {
     return { method, params, format }
   }
+
+  public static reformat<T = any>(payload: ReturnType<(typeof this)[`build`]>, format: (raw: any) => T) {
+    return {
+      ...payload,
+      format
+    }
+  }
 }
